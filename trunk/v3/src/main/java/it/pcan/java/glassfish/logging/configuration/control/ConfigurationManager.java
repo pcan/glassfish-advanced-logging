4061a0edce6f73dec2072d595a3313923a8fc005
pierocangianiello@gmail.com <pierocangianiello@gmail.com@1b951c8d-6f0d-b073-a138-ee13c47b16a1>
2013-09-30 09:36:26 +0000

    First commit for Glassfish v3.

---
+++ trunk/v3/src/main/java/it/pcan/java/glassfish/logging/configuration/control/ConfigurationManager.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2013 Pierantonio Cangianiello.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.pcan.java.glassfish.logging.configuration.control;
+
+import com.sun.enterprise.config.serverbeans.Config;
+import it.pcan.java.glassfish.logging.configuration.entity.AdvancedLoggingConfiguration;
+import it.pcan.java.glassfish.logging.configuration.entity.HandlerConfiguration;
+import it.pcan.java.glassfish.logging.configuration.entity.LoggerBinding;
+import java.beans.PropertyVetoException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
+import org.jvnet.hk2.config.ConfigBean;
+import org.jvnet.hk2.config.ConfigSupport;
+import org.jvnet.hk2.config.SingleConfigCode;
+import org.jvnet.hk2.config.TransactionFailure;
+
+/**
+ *
+ * @author Pierantonio Cangianiello
+ */
+public class ConfigurationManager {
+
+    private final static Logger logger = Logger.getLogger(ConfigurationManager.class.getName());
+    private final AdvancedLoggingConfiguration configuration;
+
+    public ConfigurationManager(AdvancedLoggingConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    public List<HandlerConfiguration> getHandlerConfigurationList() {
+        return configuration.getLogHandler();
+    }
+
+    public List<LoggerBinding> getLoggerBindingList() {
+        return configuration.getLoggerBinding();
+    }
+    
+    public void removeAllLoggerBindings() throws TransactionFailure {
+        Iterator<LoggerBinding> iterator = configuration.getLoggerBinding().iterator();
+        while (iterator.hasNext()) {
+            LoggerBinding loggerBinding = iterator.next();
+            ConfigBean loggerBindingBean = (ConfigBean) ConfigBean.unwrap(loggerBinding);
+            ConfigSupport.deleteChild(loggerBindingBean.parent(), loggerBindingBean);
+        }
+    }
+    
+    protected void removeLoggerBindings(String handlerName) throws TransactionFailure {
+        Iterator<LoggerBinding> iterator = configuration.getLoggerBinding().iterator();
+        while (iterator.hasNext()) {
+            LoggerBinding loggerBinding = iterator.next();
+            if(loggerBinding.getHandlerName().equals(handlerName)) {
+                ConfigBean loggerBindingBean = (ConfigBean) ConfigBean.unwrap(loggerBinding);
+                ConfigSupport.deleteChild(loggerBindingBean.parent(), loggerBindingBean);
+            }
+        }
+    }
+    
+    public HandlerConfiguration addHandlerConfiguration(final String name, final String fileNamePattern, final int maxFileSize, final int maxFileRotations) throws TransactionFailure {
+        return (HandlerConfiguration) ConfigSupport.apply(new SingleConfigCode<AdvancedLoggingConfiguration>() {
+            @Override
+            public Object run(AdvancedLoggingConfiguration parent) throws TransactionFailure, PropertyVetoException {
+                HandlerConfiguration child = parent.createChild(HandlerConfiguration.class);
+                child.setName(name);
+                child.setFileNamePattern(fileNamePattern);
+                child.setMaxFileSize(maxFileSize);
+                child.setMaxFileRotations(maxFileRotations);
+                parent.getLogHandler().add(child);
+                return child;
+            }
+        }, configuration);
+    }
+    
+    public LoggerBinding addLoggerBinding(final String handlerName, final String loggerName, final Boolean useParentHandlers) throws TransactionFailure {
+        return (LoggerBinding) ConfigSupport.apply(new SingleConfigCode<AdvancedLoggingConfiguration>() {
+            @Override
+            public Object run(AdvancedLoggingConfiguration parent) throws TransactionFailure, PropertyVetoException {
+                LoggerBinding child = parent.createChild(LoggerBinding.class);
+                child.setLoggerName(loggerName);
+                child.setHandlerName(handlerName);
+                child.setUseParentHandlers(useParentHandlers);
+                parent.getLoggerBinding().add(child);
+                return child;
+            }
+        }, configuration);
+    }
+
+    public void removeHandlerConfiguration(String logHandlerName) throws TransactionFailure {
+        Iterator<HandlerConfiguration> iterator = configuration.getLogHandler().iterator();
+        removeLoggerBindings(logHandlerName);
+        while (iterator.hasNext()) {
+            HandlerConfiguration handlerConfiguration = iterator.next();
+            if (handlerConfiguration.getName().equals(logHandlerName)) {
+                ConfigBean handlerConfigBean = (ConfigBean) ConfigBean.unwrap(handlerConfiguration);
+                ConfigSupport.deleteChild(handlerConfigBean.parent(), handlerConfigBean);
+            }
+        }
+    }
+
+    public void updateHandlerConfiguration(final HandlerConfiguration handlerConfiguration, final String fileNamePattern, final int maxFileSize, final int maxFileRotations) throws TransactionFailure {
+        ConfigSupport.apply(new SingleConfigCode<HandlerConfiguration>() {
+            @Override
+            public Object run(HandlerConfiguration parent) throws TransactionFailure, PropertyVetoException {
+                parent.setFileNamePattern(fileNamePattern);
+                parent.setMaxFileSize(maxFileSize);
+                parent.setMaxFileRotations(maxFileRotations);
+                return null;
+            }
+        }, handlerConfiguration);
+    }
+
+    public static AdvancedLoggingConfiguration createEmptyConfiguration(Config config) throws TransactionFailure {
+        ConfigSupport.apply(new SingleConfigCode<Config>() {
+            @Override
+            public Object run(Config parent) throws TransactionFailure {
+                AdvancedLoggingConfiguration child = parent.createChild(AdvancedLoggingConfiguration.class);
+                parent.getContainers().add(child);
+                return child;
+            }
+        }, config);
+        return config.getExtensionByType(AdvancedLoggingConfiguration.class);
+    }
+    
+    
+}
---
+++ trunk/v3/src/main/java/it/pcan/java/glassfish/logging/service/AdvancedLoggingService.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2013 Pierantonio Cangianiello.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.pcan.java.glassfish.logging.service;
+
+import com.sun.enterprise.config.serverbeans.Config;
+import com.sun.enterprise.config.serverbeans.Configs;
+import it.pcan.java.glassfish.logging.configuration.control.ConfigurationManager;
+import it.pcan.java.glassfish.logging.configuration.entity.AdvancedLoggingConfiguration;
+import it.pcan.java.glassfish.logging.configuration.entity.HandlerConfiguration;
+import it.pcan.java.glassfish.logging.configuration.entity.LoggerBinding;
+import it.pcan.java.glassfish.logging.handler.LogHandlerManager;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.glassfish.api.Startup;
+import org.glassfish.api.admin.ServerEnvironment;
+import org.jvnet.hk2.annotations.Inject;
+import org.jvnet.hk2.annotations.Scoped;
+import org.jvnet.hk2.annotations.Service;
+import org.jvnet.hk2.component.Habitat;
+import org.jvnet.hk2.component.PostConstruct;
+import org.jvnet.hk2.component.Singleton;
+import org.jvnet.hk2.config.TransactionFailure;
+
+/**
+ *
+ * @author Pierantonio Cangianiello
+ */
+@Service
+@Scoped(Singleton.class)
+public class AdvancedLoggingService implements Startup, PostConstruct {
+
+    private final static Logger logger = Logger.getLogger(AdvancedLoggingService.class.getName());
+    @Inject(name = ServerEnvironment.DEFAULT_INSTANCE_NAME)
+    private Config currentConfig;
+    @Inject
+    private static Habitat habitat;
+    @Inject
+    private static LogHandlerManager logHandlerManager;
+
+    //@Inject
+    //private Configs configs;
+    //@Inject(optional=true) //when this Module is installed, there is no configuration yet.
+    //private AdvancedLoggingConfiguration advancedLoggingConfiguration;
+    @Override
+    public Lifecycle getLifecycle() {
+        return Lifecycle.SERVER;
+    }
+
+    @Override
+    public void postConstruct() {
+        /* try {
+         Thread.sleep(5000);
+         } catch (InterruptedException ex) {
+         Logger.getLogger(AdvancedLoggingService.class.getName()).log(Level.SEVERE, null, ex);
+         }*/
+
+        String currentConfigName = currentConfig.getName();
+
+        System.out.println("\033[34mLogHandlerService module: postConstruct()");
+        System.out.println("\033[34mActive configuration: " + currentConfigName);
+
+
+        ConfigurationManager configurationManager = getConfigurationManager(currentConfigName);
+        List<HandlerConfiguration> handlerConfigurationList = configurationManager.getHandlerConfigurationList();
+        if (handlerConfigurationList != null) {
+            for (HandlerConfiguration handlerConfiguration : handlerConfigurationList) {
+                try {
+                    logHandlerManager.addLogHandler(handlerConfiguration);
+                    logger.log(Level.FINE, "Advanced Logging: Registered handler: {0}", handlerConfiguration.getName());
+                } catch (IOException ex) {
+                    logger.log(Level.WARNING, "I/O Exception: cannot create handler {0}", handlerConfiguration.getName());
+                }
+            }
+        }
+        List<LoggerBinding> loggerBindingList = configurationManager.getLoggerBindingList();
+        if(loggerBindingList != null) {
+            for (LoggerBinding loggerBinding : loggerBindingList) {
+                String handlerName = loggerBinding.getHandlerName();
+                String loggerName = loggerBinding.getLoggerName();
+                boolean useParentHandlers = Boolean.parseBoolean(loggerBinding.getUseParentHandlers());
+                logHandlerManager.bindLogger(handlerName, loggerName, useParentHandlers);
+                logger.log(Level.FINE, "Advanced Logging: Registered binding: {0} -> {1}", new Object[]{loggerName, handlerName});
+            }
+        }
+
+
+        /*List<Config> configList = configs.getConfig();
+         for (Config config : configList) {
+         AdvancedLoggingConfiguration advancedLoggingConfiguration =
+         config.getExtensionByType(AdvancedLoggingConfiguration.class);
+         if (advancedLoggingConfiguration == null) {
+         try {
+         advancedLoggingConfiguration = ConfigurationManager.createEmptyConfiguration(config);
+         } catch (TransactionFailure ex) {
+         logger.log(Level.SEVERE, null, ex);
+         }
+         }
+         configurationManagerMap.put(config.getName(), new ConfigurationManager(advancedLoggingConfiguration));
+         }*/
+        /*
+         if(advancedLoggingConfiguration == null) {
+         try {
+         advancedLoggingConfiguration = ConfigurationManager.createEmptyConfiguration(config);
+         } catch (TransactionFailure ex) {
+         logger.log(Level.SEVERE, null, ex);
+         }
+         }
+         for(HandlerConfiguration handlerConfiguration : advancedLoggingConfiguration.getLogHandler()) {
+         System.out.println("Log handler: " + handlerConfiguration.getName() + " (" + handlerConfiguration.getFileNamePattern() + ")");
+         }*/
+
+    }
+
+    public String getCurrentConfigName() {
+        return currentConfig.getName();
+    }
+
+    public ConfigurationManager getConfigurationManager(String configName) {
+        ConfigurationManager configurationManager = null;
+        if (habitat != null) {
+            Configs configs = habitat.getComponent(Configs.class);
+            Config config = configs.getConfigByName(configName);
+            AdvancedLoggingConfiguration advancedLoggingConfiguration =
+                    config.getExtensionByType(AdvancedLoggingConfiguration.class);
+            if (advancedLoggingConfiguration == null) {
+                try {
+                    advancedLoggingConfiguration = ConfigurationManager.createEmptyConfiguration(config);
+                } catch (TransactionFailure ex) {
+                    logger.log(Level.SEVERE, null, ex);
+                }
+            }
+            configurationManager = new ConfigurationManager(advancedLoggingConfiguration);
+        }
+        return configurationManager;
+    }
+}
